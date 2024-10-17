d38a1c2af1fefe06d3e418738c7caee21930c2a0
lllleeon <goodjowon@gmail.com>
2024-10-17 09:54:47 +0900

    업캐슽잉ㅂ범위제한실습

---
+++ src/ex10/ex10_4/MainSquare.java
@@ -0,0 +1,10 @@
+package ex10.ex10_4;
+
+public class MainSquare {
+    public static void main(String[] args) {
+        // object shape square 객체으 필ㄷ아메스도 접극ㄴ가능
+        Square square = new Square();
+        // shape 객체의 필드와메속드 접근가능 (예외적으로 squre객체의 맴버ㅡㄹ 접근할수도있다)
+        Shape shape = new Square();
+    }
+}
